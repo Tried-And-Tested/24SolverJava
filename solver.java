a90947a01b097e1f85302e55d62bb01e7b63f48f
Tried-And-Tested <tcheng23@binghamton.edu>
2023-08-21 13:32:36 -0400

    windows is cringe

--- solver.java
+++ solver.java
@@ -27,15 +27,17 @@ public class solver {
             Scanner scan  = new Scanner(System.in);
             System.out.println("Please input 4 numbers deliminated by spaces or q to quit");
                 String input = scan.nextLine();
-                System.out.println(input);
                 String[] inputArr = input.split(" ", 0);
+
                 if(isValid(inputArr) && inputArr.length ==4){
 
                 }
+
                 else if(input.replaceAll("\\s","").equals("q")){
                     scan.close();
                     System.exit(0);
                 }
+                
                 else{
                 }
         }
