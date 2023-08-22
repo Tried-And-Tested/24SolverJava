689b15521bb8e29dd18236a21287750c225885ab
Tried-And-Tested <tcheng23@binghamton.edu>
2023-08-22 12:27:35 -0400

    day 2 prime part 1

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # 24SolverJava
 24Solver but in Java instead of Kotlin
+
+#Background
+24 is a simple game where 4 cards are drawn and the player has to reach the number 24 using basic mathematical opertaions (+ ; - ; / ; * ). For added flavour, I will also be including factorial as one of the operations that can be used. The value of each card is the number of the card (not factoring suit value). For royals, the jack is assigned 11 and the king is assigned 13. The ace is assigned the number 1
+
+#Rules
+In the game 24, the player must reach the number 24 by using all 4 numbers given and any of the given mathematical opertaions. The player can NOT use a number if it has already been used once before (IE if the draw is [8, 6, 4, 4], the player can only use 8 once)
+
+#Idea
+My goal in this project is to write a program that can solve the game 24 with the added rules without simply brute forcing its way to the answer. To this end, I seeking to emulate how I, as a human, would play the game 24.
+
+When I play the game 24, I tend to gravitate towards using 4! since it tends to be the easier approach. I achieve this through looking for factors or dividend of 4 and solving for the respective factor/divisor before adding and subtracting
+#Methodology
+
+The way I approached this problem is by first trying to arrive at 24 through achieving 4! since 4 can be found by dividing either 12 or 8, shares many factors with 24, and is generally an easier number to add and subtract up to.
+
+To find 4, I first organize the 4 numbers in their natural order using a treeSet in order to take advantage of the guarantee that the first index always holds the smallest value. Once the set is sorted, I start searching for factors and dividends of 4. If a factor or dividend is found, then my code will start looking for factors or dividends of the factor/dividend found. If a factor or dividend is not found, then my code will try to create a factor through addition or subtraction
+
+In the case that factors or dividends can not be found such as in the case we come across a prime number like 3 or 2, my code will then take the remaining numbers and try to create the other respective factor through addition and subtraction. if no factor can be made, my code then resorts to purely adding and subtracting
+
+There is also the caveat where ending with a 4 and a 3 is a valid answer since 4 * 3! is 24
+
+In the case my code can not reach 4, it starts searching for 24 using the same steps as above. If neither case works, then my code concludes that no solution can be found
+
+#Limitations
+There are several limitations to my code due to the method I go about solving the game, such as in the case the 4 drawn numbers are [6, 6, 6, 1]. In this case, my code doesnt see the answer 6 (6 - 1) - 6 because 5 is neither a valid factor of 4 or 24, which my code relies on.
--- solver.java
+++ solver.java
@@ -1,4 +1,5 @@
 import java.util.*;
+//Notes: Treeset is always sorted via natural order unless given an overriding comparator
 
 public class solver {
     private static Boolean solve(TreeSet<Integer> inputArrNum, int goal){
@@ -9,10 +10,12 @@ public class solver {
         //if no work, try to make factors or dividends
 
         //if all else fails, add and subtract to victory
-        addSub(inputArrNum, goal);
+        Integer intArr[] = new Integer[inputArrNum.size()];
+        intArr = inputArrNum.toArray(intArr);
+        addSub(intArr, goal);
         return false;
     }
-    
+    //this searches for factors or dividends and tries to make the respective factor/divisor
     private static Boolean doMath(TreeSet<Integer> inputArrNum, int goal){
         Iterator<Integer> value= inputArrNum.iterator();
             while(value.hasNext()){
@@ -35,13 +38,14 @@ public class solver {
         }
         return false;
     }
-    
-    private static Boolean findPart2(TreeSet<Integer> snip, int part){
-        if(snip.size()==1 && snip.first()==part){
+    //creates the other factor or dividend if found in doMath
+    //part is the variable we are trying to make
+    private static Boolean findPart2(TreeSet<Integer> inputArrNum, int part){
+        if(inputArrNum.size()==1 && inputArrNum.first()==part){
             return true;
         }
-        else if(snip.size()>1){
-            Iterator<Integer> x = snip.iterator();
+        else if(inputArrNum.size()>1){
+            Iterator<Integer> x = inputArrNum.iterator();
             while(x.hasNext()){
                     int i = x.next();
                     //find a factor or dividend of the factor/dividend
@@ -49,25 +53,56 @@ public class solver {
                         int factor = part / i;
                         int divisor  = i /part;
                     //create a treeset without the factor / dividend
-                    TreeSet<Integer> piece = new TreeSet<Integer>();
-                    piece = (TreeSet<Integer>)snip.clone();
-                    piece.remove(i);
-
-                    if(findPart2(piece,factor)){
+                    TreeSet<Integer> snip = new TreeSet<Integer>();
+                    snip = (TreeSet<Integer>)inputArrNum.clone();
+                    snip.remove(i);
+                    //if we can create the factor or dividend, then we try to make it with the rest of the numbers avaliable
+                    if(findPart2(snip,factor)){
                         return true;
                     }
-                    else if(findPart2(piece,divisor)){
+                    else if(findPart2(snip,divisor)){
                         return true;
                     }
                 }
-                //if we cant find a factor or dividend of the factor/dividend
-                
+                //if we cant find a factor or dividend of the factor/dividend, we create it through addition or subtraction
+                Integer intArr[] = new Integer[inputArrNum.size()];
+                intArr = inputArrNum.toArray(intArr);
+                addSub(intArr, part);
             }
         }
         return false;
     }
-    
-    private static Boolean addSub(TreeSet<Integer> inputArrNum, int goal){
+    //takes the numbers given in the treeset and then tries to create goal using addition and subtraction
+    private static Boolean addSub(Integer[] inputArrNum, int goal){
+        //if the first element is greater, we try to subtract downwards
+        if(inputArrNum[0] > goal){
+            //smallDist is supposed to represent the smallest distance possible from goal that can be made through subtraction
+            int smallDist = Math.abs(goal - (Math.abs(inputArrNum[0]-inputArrNum[1])));
+            //smallNumIndex is the index of the other value that yeilds the smallest difference with the goal
+            int smallNumIndex = 1;
+            for(int i =2; i<inputArrNum.length; i++){
+                if( smallDist > ( Math.abs( goal - ( Math.abs(inputArrNum[0]-inputArrNum[i]) ) ) ) ){
+                   smallNumIndex = i;
+                   smallDist = ( Math.abs( goal - ( Math.abs(inputArrNum[0]-inputArrNum[i]) ) ) );
+                }
+            }
+            //if here, we've found the value of the smallest distance and the index of the number that creates it
+            
+        }
+        //if the first element is the goal and the array still has number to use, we try to make a 1 with the rest
+        else if(inputArrNum[0] == goal){
+            if(inputArrNum.length != 1){
+                Integer[] smallerSnip = Arrays.copyOfRange(inputArrNum, 1, inputArrNum.length);
+                addSub(smallerSnip, 1);
+            }
+            else{
+                return true;
+            }
+        }
+        //if the first element is less than the goal, we try to add upwards
+        else{
+
+        }
         return false;
     }
 
