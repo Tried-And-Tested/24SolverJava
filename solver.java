ca8cabab24683a18314c770face2e8f1d956c4fd
Tried-And-Tested <tcheng23@binghamton.edu>
2023-08-23 14:14:28 -0400

    end of day 3 prime part 1

--- solver.java
+++ solver.java
@@ -4,39 +4,111 @@ import java.util.*;
 //Notes as of 8/22/23 8:55pm: solve should take an array to make the 2nd condition easier and doMath might be able to
 //use a hashset to make remove faster. findPart2 might not need a treeset
 public class solver {
-    private static Boolean solve(TreeSet<Integer> inputArrNum, int goal){
+    private static Boolean solve(Integer[] inputArrNum, int goal){
         //first search for factors and dividends
         if(doMath(inputArrNum,goal)){
             return true;
         }
-        //convert the treeSet into an array in order to make the next steps easier
-        Integer intArr[] = new Integer[inputArrNum.size()];
-        intArr = inputArrNum.toArray(intArr);
         //if no work, try to make factors or dividends by going through all unique pairs in inputArrNum and checking if they
         //are either a factor or dividend
+        for(int x=0;x<inputArrNum.length;x++){
+            for(int y=0;y<inputArrNum.length;y++){
+                //if we find either a factor or dividend, we call doMath with an array of size 3 [factor, element, element]
+                //and the respective divisor / factor as part
+                Integer[] doMathInSmall = new Integer[3];
+                if((Math.abs(x-y)) % goal == 0){
+                    //if x+y is a dividend of goal
+                    doMathInSmall[0] = x+y;
+                    //space represents the other 2 empty spaces after we find the factor or dividend
+                    int space =1;
+                    for(Integer leftovers : inputArrNum){
+                        //we are looking for the other 2 unused elements and putting them in doMathInSmall
+                        //only 2 numbers can make this statement true
+                        if(leftovers != x && leftovers != y){
+                            doMathInSmall[space] = leftovers;
+                            space++;
+                        }
+                    }
 
+                    if(doMath(doMathInSmall, ((x-y)/ goal) )){
+                        return true;
+                    }
+                }
+                else if((x+y) % goal == 0){
+                    //if x+y is a dividend of goal
+                    doMathInSmall[0] = x+y;
+                    //space represents the other 2 empty spaces after we find the factor or dividend
+                    int space =1;
+                    for(Integer leftovers : inputArrNum){
+                        //we are looking for the other 2 unused elements and putting them in doMathInSmall
+                        //only 2 numbers can make this statement true
+                        if(leftovers != x && leftovers != y){
+                            doMathInSmall[space] = leftovers;
+                            space++;
+                        }
+                    }
+
+                    if(doMath(doMathInSmall, ((x+y)/ goal) )){
+                        return true;
+                    }
+                }
+                if(goal % (Math.abs(x-y)) == 0){
+                    //if x+y is a dividend of goal
+                    doMathInSmall[0] = x+y;
+                    //space represents the other 2 empty spaces after we find the factor or dividend
+                    int space =1;
+                    for(Integer leftovers : inputArrNum){
+                        //we are looking for the other 2 unused elements and putting them in doMathInSmall
+                        //only 2 numbers can make this statement true
+                        if(leftovers != x && leftovers != y){
+                            doMathInSmall[space] = leftovers;
+                            space++;
+                        }
+                    }
+
+                    if(doMath(doMathInSmall, (goal / (x-y)) )){
+                        return true;
+                    }
+                }
+                else if(goal % (x+y) == 0){
+                    //if x+y is a dividend of goal
+                    doMathInSmall[0] = x+y;
+                    //space represents the other 2 empty spaces after we find the factor or dividend
+                    int space =1;
+                    for(Integer leftovers : inputArrNum){
+                        //we are looking for the other 2 unused elements and putting them in doMathInSmall
+                        //only 2 numbers can make this statement true
+                        if(leftovers != x && leftovers != y){
+                            doMathInSmall[space] = leftovers;
+                            space++;
+                        }
+                    }
+
+                    if(doMath(doMathInSmall, (goal / (x+y)) )){
+                        return true;
+                    }
+                }
+            }
+        }
         //if all else fails, add and subtract to victory
-        addSub(intArr, goal);
+        addSub(inputArrNum, goal);
         return false;
     }
     
     //this searches for factors or dividends and tries to make the respective factor/divisor
-    private static Boolean doMath(TreeSet<Integer> inputArrNum, int goal){
-        Iterator<Integer> value= inputArrNum.iterator();
-            while(value.hasNext()){
-                int x = value.next();
-                if((goal % x == 0) || (x % goal == 0)){
-                    int factor = goal / x;
-                    int divisor  = x /goal;
+    private static Boolean doMath(Integer[] inputArrNum, int goal){
+        for(Integer x : inputArrNum){
+            if((goal % x == 0) || (x % goal == 0)){
+                int factor = goal / x;
+                int divisor  = x /goal;
                 //create a treeset without the factor / dividend
-                TreeSet<Integer> snip = new TreeSet<Integer>();
-                snip = (TreeSet<Integer>)inputArrNum.clone();
-                snip.remove(x);
+                TreeSet<Integer> part2Snip = new TreeSet<Integer>(Arrays.asList(inputArrNum));
+                part2Snip.remove(x);
 
-                if(findPart2(snip,factor)){
+                if(findPart2(part2Snip,factor)){
                     return true;
                 }
-                if(findPart2(snip,divisor)){
+                if(findPart2(part2Snip,divisor)){
                     return true;
                 }
             }
@@ -58,22 +130,20 @@ public class solver {
                     if((part % i == 0) || (i % part == 0)){
                         int factor = part / i;
                         int divisor  = i /part;
-                    //create a treeset without the factor / dividend
-                    TreeSet<Integer> snip = new TreeSet<Integer>();
-                    snip = (TreeSet<Integer>)inputArrNum.clone();
-                    snip.remove(i);
+                    //remove the factor from the list of unused numbers
+                    inputArrNum.remove(i);
                     //if we can create the factor or dividend, then we try to make it with the rest of the numbers avaliable
-                    if(findPart2(snip,factor)){
+                    if(findPart2(inputArrNum,factor)){
                         return true;
                     }
-                    else if(findPart2(snip,divisor)){
+                    else if(findPart2(inputArrNum,divisor)){
                         return true;
                     }
                 }
                 //if we cant find a factor or dividend of the factor/dividend, we create it through addition or subtraction
-                Integer intArr[] = new Integer[inputArrNum.size()];
-                intArr = inputArrNum.toArray(intArr);
-                addSub(intArr, part);
+                Integer addSubIn[] = new Integer[inputArrNum.size()];
+                addSubIn = inputArrNum.toArray(addSubIn);
+                addSub(addSubIn, part);
             }
         }
         return false;
@@ -88,6 +158,7 @@ public class solver {
             //smallNumIndex is the index of the other value that yeilds the smallest difference with the goal
             int smallNumIndex = 1;
             for(int i =2; i<inputArrNum.length; i++){
+                //we always want the smallest possible distance from the goal
                 if( smallDist > ( Math.abs( goal - ( Math.abs(inputArrNum[0]-inputArrNum[i]) ) ) ) ){
                    smallNumIndex = i;
                    smallDist = ( Math.abs( goal - ( Math.abs(inputArrNum[0]-inputArrNum[i]) ) ) );
@@ -118,6 +189,7 @@ public class solver {
             int smallDist = Math.abs(goal - (Math.abs(inputArrNum[0]+inputArrNum[1])));
             int smallNumIndex =1;
             for(int i =2; i<inputArrNum.length; i++){
+                //we always want the smallest possible distance from the goal
                 if( smallDist > ( Math.abs( goal - ( Math.abs(inputArrNum[0]+inputArrNum[i]) ) ) ) ){
                    smallNumIndex = i;
                    smallDist = ( Math.abs( goal - ( Math.abs(inputArrNum[0]+inputArrNum[i]) ) ) );
@@ -134,7 +206,7 @@ public class solver {
         //if there is a single non digit character, parseInt returns an exception
         try{
             check = check.replaceAll("\\s+","");
-            int filter = Integer.parseInt(check);
+            Integer.parseInt(check);
             return true;
         }
         catch(Exception e){
@@ -148,14 +220,14 @@ public class solver {
             Scanner scan  = new Scanner(System.in);
             System.out.println("Please input 4 numbers deliminated by spaces or q to quit");
                 String input = scan.nextLine();
-
-                if(isValid(input) && input.length() ==4){
+                //isValid takes the user input as a string, removes the white space, then parses the result into an int
+                //7 is the smallest length allowed since [w x y z] is the smallest valid input
+                if(isValid(input) && input.length() <= 7){
                     //create an array that consists only of non whitespace characters
                     String[] inputArr = input.split("\\s+", 0);
-                    TreeSet<Integer> inputArrNum = new TreeSet<Integer>();
-
-                    for(String entry : inputArr){
-                        inputArrNum.add(Integer.parseInt(entry));
+                    Integer[] inputArrNum = new Integer[inputArr.length];
+                    for(int i=0;i<inputArr.length;i++){
+                        inputArrNum[i]=Integer.parseInt(inputArr[i]);
                     }
                     //if both tests fail, we declare no solution
                     if(!solve(inputArrNum,4) && !solve(inputArrNum,24)){
